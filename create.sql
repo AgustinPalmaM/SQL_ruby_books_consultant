d265fa48b6f9e94ec6756947ae9dd95b016a335d
AgustinPalmaM <palma.agustin7@gmail.com>
2023-06-28 17:57:28 -0400

    ADD sql sentences into crete.sql to create tables authors, publishers, genres, books, books_genres

--- create.sql
+++ create.sql
@@ -11,3 +11,22 @@ CREATE TABLE publishers(
   annual_revenue INT NOT NULL,
   founded_year INT NOT NULL
 );
+
+CREATE TABLE genres(
+  id SERIAL PRIMARY KEY NOT NULL,
+  genre VARCHAR NOT NULL
+);
+
+CREATE TABLE books(
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR NOT NULL,
+  pages INT NOT NULL,
+  author_id INT REFERENCES authors(id) NOT NULL,
+  publisher_id INT REFERENCES publishers(id) NOT NULL
+);
+
+CREATE TABLE books_genres(
+  id SERIAL PRIMARY KEY NOT NULL,
+  genre_id INT REFERENCES genres(id) NOT NULL,
+  book_id INT REFERENCES books(id) NOT NULL
+);
